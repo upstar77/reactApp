7311556fda973365a849f897b39cb41278a5045b
Up Star <upstar77@hotmail.com>
2018-03-01 20:24:45 +1000

    Implement React Router

---
+++ Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Login</h2>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
-var config = {
+module.exports = {
     entry: './main.js',
     output: {
-        path: '/',
-        filename: 'index.js',
+        filename: 'bundle.js',
     },
     devServer: {
-        inline: true,
-        port: 8080
+        port: 7777,
     },
+
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
             }
         ]
     }
 }
-
-module.exports = config;
