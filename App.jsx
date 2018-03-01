bc295537e15075a1bbd8e45394be7827aaff78ac
Up Star <upstar77@hotmail.com>
2018-03-01 22:49:52 +1000

    Adding animation

--- App.jsx
+++ App.jsx
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: ['Item 1...', 'Item 2...', 'Item 3...', 'Item 4...']
+        };
+
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd() {
+        var newItems = this.state.items.concat([prompt('Create New Item')]);
+        this.setState({items: newItems});
+    }
+
+    handleRemove(i) {
+        var newItems = this.state.items.slice();
+        newItems.splice(i, 1);
+        this.setState({items: newItems});
+    }
 
     render() {
-        return (
-            <Router>
-                <div>
-                    <h2>Welcome to React Router</h2>
-                    <ul>
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/Login'}>Login</Link></li>
-                    </ul>
-                    <hr />
-
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route exact path='/Login' component={Login} />
-
-                    </Switch>
+        var items = this.state.items.map(function(item, i) {
+            return (
+                <div key={item} onClick={this.handleRemove.bind(this, i)}>
+                    {item}
                 </div>
-            </Router>
+            );
+        }.bind(this));
+
+        return (
+            <div>
+                <button onClick={this.handleAdd}>Add Item</button>
+
+                <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={2500} transitionLeaveTimeout={500}>
+                    {items}
+                </ReactCSSTransitionGroup>
+            </div>
         );
     }
 }
